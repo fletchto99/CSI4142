505789878066ffbacf6738ae70e8450dcfb3cf9d
MetaDark <kurt.bruneau@gmail.com>
2017-04-02 14:43:33 -0400

    Updated schema

--- schema.sql
+++ schema.sql
@@ -1,37 +1,37 @@
-CREATE TABLE ProductPrice (
-  date int references Date,
-  product int references Product,
-  location int references Location,
-  price numeric(2),
+CREATE TABLE date (
+  id int PRIMARY KEY,
+  date date NOT NULL,
+  day int NOT NULL CHECK (day >= 0 AND day <= 6),
+  week int NOT NULL CHECK (week >= 0 AND week <= 52),
+  month int NOT NULL CHECK (month > 0 AND month <= 12),
+  year int NOT NULL,
+  weekend boolean NOT NULL
 );
 
-CREATE TABLE Product (
+CREATE TABLE product (
   id int PRIMARY KEY,
-  name text,
-  category text,
-  energy int,
-  carbohydrates int,
-  fat int,
-  protein int,
+  name text NOT NULL,
+  category text NOT NULL,
+  energy int NOT NULL,
+  carbohydrates int NOT NULL,
+  fat int NOT NULL,
+  protein int NOT NULL
 );
 
-CREATE TABLE Date (
+CREATE TABLE location (
   id int PRIMARY KEY,
-  date date,
-  day int, -- 0-6
-  week int, -- 0-52
-  month int, -- 0-12
-  year int,
-  weekend boolean,
+  type int NOT NULL,
+  city text NOT NULL,
+  country text NOT NULL,
+  gdp int NOT NULL,
+  population int NOT NULL,
+  life_expectancy int NOT NULL,
+  average_income numeric(2) NOT NULL
 );
 
-CREATE TABLE Location (
-  id int PRIMARY KEY,
-  type int,
-  city text,
-  country text,
-  gdp int,
-  population int,
-  life_expectancy int,
-  average_income numeric(2),
+CREATE TABLE product_price (
+  date int NOT NULL REFERENCES date (id),
+  product int NOT NULL REFERENCES product (id),
+  location int NOT NULL REFERENCES location (id),
+  price numeric(2) NOT NULL
 );
