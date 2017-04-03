d2a568af2f372187dce4a4e0ce6a1fe3c299cd1e
MetaDark <kurt.bruneau@gmail.com>
2017-04-02 20:45:13 -0400

    Import products

--- import.py
+++ import.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import csv
 import psycopg2
+import csv
+from collections import defaultdict
 
 dbconfig = {
     'dbname': 'postgres',
@@ -11,44 +12,66 @@ dbconfig = {
 conn = psycopg2.connect(**dbconfig)
 cur = conn.cursor()
 
-dates = {}
-products = {}
-locations = {}
-product_prices = []
+dates = defaultdict(dict)
+products = defaultdict(dict)
+locations = defaultdict(dict)
+product_prices = defaultdict(dict)
+
+avg_annual_income = defaultdict(dict)
+gdp = defaultdict(dict)
+life_expectancy = defaultdict(dict)
+population = defaultdict(dict)
 
 with open('datasets/food-prices.csv') as f:
-    reader = csv.DictReader(f)
+    reader = csv.reader(f, delimiter=',', quotechar='"')
+    head = dict(reversed(field) for field in enumerate(next(reader)))
 
     for row in reader:
-        date = row['Obs Date (yyyy-MM-dd)']
-        if not date in dates:
-            dates[date] = True
+        date = dates[row[head['Obs Date (yyyy-MM-dd)']]]
+        date['date'] = row[head['Obs Date (yyyy-MM-dd)']]
 
-        product = row['Product Code']
-        if not product in products:
-            products[product] = {
-                'id': product,
-                'name': row['Product Name']
-            }
+        product = products[row[head['Product Code']]]
+        product['id'] = row[head['Product Code']]
+        product['name'] = row[head['Product Name']]
 
-        location = row['Location Code']
-        if not location in locations:
-            locations[location] = {
-                'id': location,
-                'type': None,
-                'city': row['Location Name'],
-                'country': row['Country'],
-                'gdp': None,
-                'population': None,
-                'life_expectancy': None,
-                'average_income': None
-            }
+        location = locations[row[head['Location Code']]]
+        location['id'] = row[head['Location Code']]
+        location['name'] = row[head['Location Name']]
+        location['country'] = row[head['Country']]
 
-        product_prices.append({
-            'date': date,
-            'product': product,
-            'location': location,
-            'price': row['Obs Price']
-        })
+        price = product_prices[(
+            row[head['Obs Date (yyyy-MM-dd)']],
+            row[head['Product Code']],
+            row[head['Location Code']]
+        )]
 
-print(product_prices)
+        price['date'] = row[head['Obs Date (yyyy-MM-dd)']];
+        price['product'] = row[head['Product Code']];
+        price['location'] = row[head['Location Code']];
+        price['price'] = row[head['Obs Price']];
+
+# Insert products
+cur.executemany("""
+INSERT INTO product (
+  id,
+  name,
+  category,
+  energy,
+  carbohydrates,
+  fat,
+  protein
+) VALUES (
+  %(id)s,
+  %(name)s,
+  '',
+  0,
+  0,
+  0,
+  0
+)
+ON CONFLICT (id) DO
+UPDATE SET
+  name = %(name)s
+""", products.values())
+
+conn.commit()
