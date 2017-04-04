b4ee8bc57b5dc818ef7e3b6df61c1c285e98b81b
Matt Langlois <fletchto99@gmail.com>
2017-04-03 23:43:39 -0400

    Minor commenting

--- 1. physical model/schema.sql
+++ 1. physical model/schema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8c99d9b3d26359cdfbdecf41279e12a870b0bb937c69008fc796a457b5bcbbb
-size 980
+oid sha256:f581c7ff726fe00f55986732f1c1b4165ce2fc5586c400ee23edb1fc0a2dcb1e
+size 1059
